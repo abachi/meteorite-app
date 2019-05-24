c79144d0d99899ff9ed8ebdb70e08cd658a833c4
Nasser Abachi <abachinasser@gmail.com>
2019-05-24 11:49:57 +0100

    change StaticAlert component width for small devices

--- src/components/StaticAlert.js
+++ src/components/StaticAlert.js
@@ -3,7 +3,7 @@ import React from 'react';
 class StaticAlert extends React.Component {
   render(){
     return(
-      <div className="w-1/4 mx-auto mt-4 bg-white p-2 rounded text-center">
+      <div className="w-3/4 sm:w-1/4 mx-auto mt-4 bg-white p-2 rounded text-center">
         <p className="text-red-500">{this.props.text}</p>
       </div>
     );
