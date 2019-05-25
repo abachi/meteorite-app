80c9b64fa9a2300f3a89bd004882b698c52fe0b4
Nasser Abachi <abachinasser@gmail.com>
2019-05-25 17:49:13 +0100

    remove THH:MM:SS format from the year

--- src/components/Meteorite.js
+++ src/components/Meteorite.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 class Meteorite extends React.Component {
 
+  format(strDate = ''){
+    if(strDate.length === 0)
+      return;
+
+    return strDate.slice(0, 10);
+  }
+
   render(){
     return(
       <tr className="m-2 border-b hover:bg-yellow-400">
@@ -11,7 +18,7 @@ class Meteorite extends React.Component {
         <td className='p-2'>{this.props.data.recclass}</td>  
         <td className='p-2'>{this.props.data.mass}</td>  
         <td className='p-2'>{this.props.data.fall}</td>  
-        <td className='p-2'>{this.props.data.year}</td>  
+        <td className='p-2'>{this.format(this.props.data.year)}</td>  
         <td className='p-2'>{this.props.data.reclat}</td>  
         <td className='p-2'>{this.props.data.reclong}</td>  
       </tr>
